3298f6de21b993988605cb788242682e78d02de0
joel <sngjojo1@gmail.com>
2026-09-13 00:29:41 +0800

    Requirement 2: COntinuous loop for stock quantity

--- auditor.py
+++ auditor.py
@@ -2,3 +2,11 @@
 inventory = 0
 failed_entries = 0
 
+while True:
+    stock = input("Enter stock quantity (or 'quit' to exit): ")
+
+    if stock.lower() == "quit":
+        break
+
+    inventory = int(stock)
+    print("Stock quantity:", inventory)
